615dc05b2f428ec1bbe57895c8488ba55687ed2b
gotsushima <gotsushima@gmail.com>
2024-06-22 10:33:00 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import PlanPicker from './components/PlanPicker.vue'
+import { ref } from 'vue'
+const show = ref(true)
+</script>
+
+<template>
+  <div class="content">
+    <h1 class="title">Coffee Plans</h1>
+
+    <h2 class="subtitle">
+      We travel the world to source the very best single origin coffee for you
+    </h2>
+
+    <PlanPicker v-if="show" />
+  </div>
+</template>
---
+++ src/components/CoffeePlan.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'Default Plan',
+    validator(value) {
+      return value.startsWith('The')
+    }
+  },
+  selected: { type: Boolean, default: false }
+})
+
+const emit = defineEmits({
+  selected(payload) {
+    return typeof payload === 'string'
+  }
+})
+
+function selectPlan() {
+  emit('selected', props.name)
+}
+</script>
+<template>
+  <div @click="selectPlan" class="plan" :class="{ 'active-plan': selected }">
+    <div class="description">
+      <span class="title">{{ name }} </span>
+    </div>
+  </div>
+</template>
